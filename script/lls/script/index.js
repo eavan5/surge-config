14dfc221e562187fd1500a6208f293d21301d531
wumao <webwwy@gmail.com>
2023-01-08 10:51:04 +0800

    1

--- script/lls/script/index.js
+++ script/lls/script/index.js
@@ -1,17 +1,17 @@
-var var_body = $response['body']
-var var_url = $request['url']
-if (var_url['indexOf']('/api/v2/member/subscription') != -0x1) {
-	var obj = {}
-	obj['expiredAt'] = 0x70db6800
-	obj['remainDays'] = 0x3e7
-	obj['active'] = !![]
-	obj['startedAt'] = 0x61ec29ff
-	var _0x57dba3 = obj
-	var_body = JSON['stringify'](_0x57dba3)
+var _0x5c3696 = $response['body']
+var _0x3d6a05 = $request['url']
+if (_0x3d6a05['indexOf']('/api/v2/member/subscription') != -0x1) {
+	var _0xab86ec = {}
+	_0xab86ec['expiredAt'] = 0x70db6800
+	_0xab86ec['remainDays'] = 0x3e7
+	_0xab86ec['active'] = !![]
+	_0xab86ec['startedAt'] = 0x61ec29ff
+	var _0x57dba3 = _0xab86ec
+	_0x5c3696 = JSON['stringify'](_0x57dba3)
 	var _0x41be63 = {}
-	_0x41be63['body'] = var_body
+	_0x41be63['body'] = _0x5c3696
 	$done(_0x41be63)
-} else if (var_url['indexOf']('/api/v2/user_goods_subscriptions/overall') != -0x1) {
+} else if (_0x3d6a05['indexOf']('/api/v2/user_goods_subscriptions/overall') != -0x1) {
 	var _0x172290 = {}
 	_0x172290['startedAt'] = 0x61e2ef80
 	_0x172290['category'] = 0x4
@@ -24,9 +24,9 @@ if (var_url['indexOf']('/api/v2/member/subscription') != -0x1) {
 	var _0x21883a = {}
 	_0x21883a['items'] = [_0x172290]
 	var _0x57dba3 = _0x21883a
-	var_body = JSON['stringify'](_0x57dba3)
+	_0x5c3696 = JSON['stringify'](_0x57dba3)
 	var _0x387301 = {}
-	_0x387301['body'] = var_body
+	_0x387301['body'] = _0x5c3696
 	$done(_0x387301)
 }
 $done()
